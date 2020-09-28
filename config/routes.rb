83732ac32f482610fa91b455bf979d4b1b646cc7
kaji24 <alohaaa0204@gmail.com>
2020-09-28 21:42:39 +0900

    route_add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'copy#sentence'
-  get "/sentence" => "copy#sentence"
+  
   post "/decide" => "copy#decide"
 end
